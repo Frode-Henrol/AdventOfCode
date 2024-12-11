9c3791823243831e4c460e8256d1b0b63a396f1d
Frode-Henrol <glock.linstow@gmail.com>
2024-12-11 16:29:55 +0100

    ad11

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-import csv
+import math
 
+def get_digit_length(number: int):
+    if number == 0:
+        return 1
+    return math.floor(math.log10(abs(number))) + 1
 
-print("gel")
+def split_number(number: int):
+    
+    num_digits = get_digit_length(number)
+    divisor = 10 ** (num_digits // 2)
+    
+    first_half = number // divisor
+    second_half = number % divisor
+    
+    return first_half, second_half
+
+number = 1000
+
+print(split_number(number))
